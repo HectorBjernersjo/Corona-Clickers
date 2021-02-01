e44634a1331364cd464f198122eb17c0154eeb46
IronicDoll <emma.alagic@outlook.com>
2021-02-01 22:46:11 +0100

    Epic text

--- Assets/hacker.cs
+++ Assets/hacker.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class hacker : MonoBehaviour
+
 {
     static int infected;
+    public Text Textis;
     public void Knappis()
+
     {
-        infected += 1; 
+        infected += 1;
+
+        Textis.text = "Infected: " +  infected.ToString();
 
     }
 
@@ -16,7 +22,7 @@ public class hacker : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    // "nej stopp hihi"
     void Update()
     {
         
